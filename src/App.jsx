9328115942a5b646596201aa4fe2eecbe508d214
tubi55 <tubi55@nate.com>
2024-05-17 13:11:36 +0900

    base 레이어 등록

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   ];
 
   return (
-    <main className="w-full min-h-screen  bg-black/5 py-[10vh] px-[10vw]">
+    <main>
       <h1 className="w-full mb-20 text-6xl font-thin text-center">Members</h1>
 
       <section>
