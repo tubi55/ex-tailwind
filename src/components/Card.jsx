fba2f490861bc368b7e76b2f61cf418ad1d88f29
tubi55 <tubi55@nate.com>
2024-06-03 17:46:56 +0900

    twMerge로 덮어쓰기는 가능

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,12 @@ import Btn from "./Btn";
 import Info from "./Info";
 import MotionBg from "./MotionBg";
 import Thumbnail from "./Thumbnail";
-import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 function Card({ data: { name, pic, position }, className }) {
   console.log(className);
   return (
-    <article className={clsx("group card rounded-xl", className)}>
+    <article className={twMerge("group card rounded-xl", className)}>
       <MotionBg />
       <Thumbnail pic={pic} />
       <Info name={name} position={position} />
