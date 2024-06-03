e749572f8aacf2d0c3e982d3bab70051c3d47947
tubi55 <tubi55@nate.com>
2024-06-03 17:46:12 +0900

    index.css가 아닌 jsx에 직접 className으로 지정한 값을 덮어쓰기 불가능

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ import clsx from "clsx";
 function Card({ data: { name, pic, position }, className }) {
   console.log(className);
   return (
-    <article className={clsx("group card", className)}>
+    <article className={clsx("group card rounded-xl", className)}>
       <MotionBg />
       <Thumbnail pic={pic} />
       <Info name={name} position={position} />
